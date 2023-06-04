8fd1e445e278cb47453663b962ac1d40d4f1acf7
Gennadiy Koganovich <genakoganovich@gmail.com>
2023-06-04 23:44:08 +0300

    0009: 2.3.3.trekking3.py start

--- 2.Spreadsheets/2.2.one_sheet/2.2.1.region_salary.py
+++ 2.Spreadsheets/2.2.one_sheet/2.2.1.region_salary.py
@@ -1,7 +1,7 @@
 import xlrd, statistics as st
 import wget
 
-# url = ' https://stepik.org/media/attachments/lesson/245267/salaries.xlsx'
+# url = 'https://stepik.org/media/attachments/lesson/245267/salaries.xlsx'
 # wget.download(url)
 
 wb = xlrd.open_workbook('salaries.xlsx')
--- 2.Spreadsheets/2.3.some_sheets/2.3.3.trekking3.py
+++ 2.Spreadsheets/2.3.some_sheets/2.3.3.trekking3.py
@@ -3,10 +3,10 @@ import math
 import xlrd, statistics as st
 import wget
 
-# url = 'https://stepik.org/media/attachments/lesson/245290/trekking2.xlsx'
+# url = 'https://stepik.org/media/attachments/lesson/245290/trekking3.xlsx'
 # wget.download(url)
 
-wb = xlrd.open_workbook('trekking2.xlsx')
+wb = xlrd.open_workbook('trekking3.xlsx')
 
 
 def replace(row):
@@ -22,6 +22,14 @@ def get_layout(sheet):
     return [sheet.row_values(i) for i in range(1, sheet.nrows)]
 
 
+def get_layout_days(sheet):
+    layout = {}
+    for i in range(1, sheet.nrows):
+        layout.setdefault(sheet.cell_value(i, 0), list())
+        layout[sheet.cell_value(i, 0)] = layout[sheet.cell_value(i, 0)].append(sheet.row_values(i, 1))
+    return layout
+
+
 def get_result(table, layout):
     cal = p = f = c = 0
     for item in layout:
@@ -33,4 +41,7 @@ def get_result(table, layout):
     return list(map(lambda x: str(math.floor(x)) , [cal, p, f, c]))
 
 
-print(' '.join(get_result(get_table(wb.sheet_by_index(0)), get_layout(wb.sheet_by_index(1)))))
+# print(' '.join(get_result(get_table(wb.sheet_by_index(0)), get_layout(wb.sheet_by_index(1)))))
+
+
+get_layout_days(wb.sheet_by_index(1))
