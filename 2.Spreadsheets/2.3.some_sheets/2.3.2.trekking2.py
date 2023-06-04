e86c3b55f962613af25d1d725152f477c64230b7
Gennadiy Koganovich <genakoganovich@gmail.com>
2023-06-04 22:44:52 +0300

    0008: 2.3.2.trekking2.py

---
+++ 2.Spreadsheets/2.3.some_sheets/2.3.2.trekking2.py
@@ -0,0 +1,36 @@
+import math
+
+import xlrd, statistics as st
+import wget
+
+# url = ' https://stepik.org/media/attachments/lesson/245267/salaries.xlsx'
+# wget.download(url)
+
+wb = xlrd.open_workbook('trekking2.xlsx')
+
+
+def replace(row):
+    return list(map(lambda x: 0 if x == '' else x, row))
+
+
+def get_table(sheet):
+    return {sheet.cell_value(i, 0): replace(sheet.row_values(i, 1))
+            for i in range(1, sheet.nrows)}
+
+
+def get_layout(sheet):
+    return [(sheet.cell_value(i, 0), sheet.cell_value(i, 1)) for i in range(1, sheet.nrows)]
+
+
+def get_result(table, layout):
+    cal = p = f = c = 0
+    for item in layout:
+        cal += table[item[0]][0] * item[1] / 100
+        p += table[item[0]][1] * item[1] / 100
+        f += table[item[0]][2] * item[1] / 100
+        c += table[item[0]][3] * item[1] / 100
+
+    return list(map(lambda x: str(math.floor(x)) , [cal, p, f, c]))
+
+
+print(' '.join(get_result(get_table(wb.sheet_by_index(0)), get_layout(wb.sheet_by_index(1)))))
